463eb5a7dda461d44e420bb247f03892875846ac
Jasper Huting <jasper.huting@gmail.com>
2021-04-03 13:33:03 +0200

    Fixed some bugs: 100% is nu positief, als je in het weekend werkt wordt er niet van je verwacht 8 uur te maken, er ging iets mis met de urenberekening per maand, is nu gefixed

--- options.js
+++ options.js
@@ -3,22 +3,32 @@ function save_options() {
     const hide_discount = document.getElementById('hide_discount').checked;
     const hide_visible = document.getElementById('hide_visible').checked;
     const hide_billable = document.getElementById('hide_billable').checked;
+    const customer = document.getElementById("customer").selectedOptions[0]
+      .value;
+    const proceeding = document.getElementById("proceeding").selectedOptions[0]
+      .value;
+      
     // const workingHours = document.getElementById('workingHours').selectedOptions[0].value;
     // const workingDays = document.getElementById('workingDays').selectedOptions[0].value;
-    chrome.storage.local.set({
+    chrome.storage.local.set(
+      {
         hide_discount: hide_discount,
         hide_visible: hide_visible,
         hide_billable: hide_billable,
+        customer: customer,
+        proceeding: proceeding,
         // workingHours: workingHours,
         // workingDays: workingDays
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
-    });
+      },
+      function () {
+        // Update status to let user know options were saved.
+        var status = document.getElementById("status");
+        status.textContent = "Options saved.";
+        setTimeout(function () {
+          status.textContent = "";
+        }, 750);
+      }
+    );
   }
   
   // Restores select box and checkbox state using the preferences
@@ -30,6 +40,8 @@ function save_options() {
         hide_discount,
         hide_visible,
         hide_billable,
+        customer,
+        proceeding,
         // workingHours,
         // workingDays
       },
@@ -37,7 +49,9 @@ function save_options() {
         document.getElementById("hide_discount").checked = items.hide_discount;
         document.getElementById("hide_visible").checked = items.hide_visible;
         document.getElementById("hide_billable").checked = items.hide_billable;
-        // document.getElementById('workingHours').value = items.workingHours;
+        document.getElementById("customer").value = items.customer;
+        document.getElementById("proceeding").value = items.proceeding;
+        // document.getElementById('proceeding').value = items.workingHours;
         // document.getElementById('workingDays').value = items.workingDays;
       }
     );
